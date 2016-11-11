0799456b41e57fe108bfa32b59d48e258767c215
Shreyas Moudgalya <smoudgal@hawk.iit.edu>
2016-11-10 22:48:15 -0600

    init 0.2

--- app.py
+++ app.py
@@ -7,9 +7,24 @@ import sys
 
 table = []
 
+def main():
+	try:
+		a = pyslurm.job()
+		jobs = a.get()
+		job_size = len(jobs)
+		if (job_size > 0):
+			display(jobs)
+			njobs = len(jobs)
+			pending = a.find('job_state', 'PENDING')
+		else:
+			print("No jobs found !")
 
-@Override
-def display(job_dict):
+	except ValueError as e:
+		print("Job query failed - {0}".format(e.args[0]))
+	
+
+
+def displayJobDict(job_dict):
     if job_dict:
         for key, value in sorted(job_dict.iteritems()):
             print("JobID {0} :".format(key))
@@ -35,25 +50,25 @@ def solve(a, power_capacity,job_size):
 
 
 
-def getCell(j, i):
-    if (i < 0 || j < 0):
+def getCell(row, col):
+    if (col < 0 || row < 0):
         return 0
 
-    item = jobs[i]
+    item = jobs[col]
 
     within=None
     without=None
-    cell = table[j][i]
+    cell = table[row][col]
 
         #If not memoized
         if (cell == -1):
-            if (item.getTotalPowerForRacks() > j): #Change
+            if (item.getTotalPowerForRacks() > row): #Change
                 within = -1
             else:
-                within = item.getNumRacks() + getCell(j - (int)item.getTotalPowerForRacks(), i - 1) #Change
+                within = item.getNumRacks() + getCell(row - (int)item.getTotalPowerForRacks(), i - 1) #Change
 
 
-            without = getCell(j, i - 1);
+            without = getCell(row, col-1);
 
             #Check to make sure we're within the value limit.
             if (within > num_nodes_capacity):
@@ -61,7 +76,7 @@ def getCell(j, i):
 
             cell = max(within, without)
             #memoized
-            table[j][i] = cell
+            table[row][col] = cell
 
     return cell
 
@@ -73,18 +88,4 @@ def traceTable():
 
 
 if __name__ == "__main__":
-
-    try:
-        a = pyslurm.job()
-        jobs = a.get()
-        job_size = len(jobs)
-        if job_size > 0:
-
-            display(jobs)
-			njobs = len(jobs)
-
-            pending = a.find('job_state', 'PENDING')
-        else:
-            print("No jobs found !")
-    except ValueError as e:
-print("Job query failed - {0}".format(e.args[0]))
+	main()
